3b9f59c789b6508e7d5460a6069c53de178fc12a
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-08-30 08:27:20 +0700

    feat: setup tailwind and implementation
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import "../app.css";
+</script>
+
+
+<div class="container">
+  <slot/>
+</div>
+
+
+<style>
+  .container {
+    @apply flex justify-center items-center w-full min-h-screen;
+  }
+</style>
